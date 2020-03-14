9c0b1bfe40923be9678da073d958f75b50e4bf7d
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-03-14 16:17:15 -0700

    made it so only the first 10 students display

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ FSJS project 2 - List Filter and Pagination
 // I used the study guide for this project as a basis for my code - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 //variable to store the student list items as an array
-const listItems = document.querySelectorAll(".student-details");
+const listItems = document.querySelectorAll(".student-item");
 //variable to store the number of items displayed on each page
 const itemsPerPage = 10; 
 
@@ -14,12 +14,13 @@ const itemsPerPage = 10;
 //showPage function to show only the items meant to be shown
 function showPage(list, page) {
    let startIndex = (page*itemsPerPage) - itemsPerPage;
-   let endIndex = (page*itemsPerPage);
-   for (let i = 0; i < listItems.length; i +=1){
+   let endIndex = (page*itemsPerPage) - 1;
+   for (let i = 0; i < list.length; i +=1){
       //appending items to show to the DOM by using an if statement to select items based on their index value
-      if (listItems[i] >= startIndex && listItems[i] <= endIndex){
-         let ul = document.querySelector(".student-list");
-         ul.appendChild(listItems[i]);
+      if (i >= startIndex && i <= endIndex){
+         list[i].style.display = 'block';
+         } else {
+         list[i].style.display = 'none';
          }
     }
    }
@@ -50,4 +51,4 @@ function appendPageLinks (list){
 }
 
 showPage(listItems, 1);
-appendPageLinks(listItems);
+
